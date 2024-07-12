834b8404d124ab651686200738825010e9b64ffc
BMarin <bryanmarin2003@hotmail.com>
2024-07-12 16:34:17 -0500

    feat: Añadido el TS para la validación del formulario

--- src/app/components/datos-form/datos-form.component.ts
+++ src/app/components/datos-form/datos-form.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { SalaryCalculationResponse } from '../../models/salary-calculation-response';
 
 @Component({
   selector: 'app-datos-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './datos-form.component.html',
-  styleUrl: './datos-form.component.css'
+  styleUrl: './datos-form.component.css',
 })
 export class DatosFormComponent {
+  employeeForm: FormGroup;
+  salaryResult: SalaryCalculationResponse | null = null;
+  loginErrorMessage: string | null = null;
 
+  constructor(private fb: FormBuilder) {
+    this.employeeForm = this.fb.group({
+      name: ['', Validators.required],
+      hourlyWage: [
+        '',
+        [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)],
+      ],
+      hoursWorked: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+      overtimeHours: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+    });
+  }
+
+  isInvalid(controlName: string): boolean {
+    const control = this.employeeForm.get(controlName);
+    return control
+      ? control.invalid && (control.dirty || control.touched)
+      : false;
+  }
 }
