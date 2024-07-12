12b3b16fcf515c0b220c81eff6f92b5338e0655d
BMarin <bryanmarin2003@hotmail.com>
2024-07-12 16:26:19 -0500

    feat: Implementación de models solicitados y creación de service y componente para los datos del usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DatosFormComponent } from './components/datos-form/datos-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component:DatosFormComponent}
+];
