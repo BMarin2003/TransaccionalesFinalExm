56095da9ef98f2b9ad091b7a03e945b2754d853d
BMarin <bryanmarin2003@hotmail.com>
2024-07-12 16:39:34 -0500

    feat: Terminado el service

--- src/app/services/calc-salary.service.ts
+++ src/app/services/calc-salary.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { SalaryCalculationRequest } from '../models/salary-calculation-request';
+import { SalaryCalculationResponse } from '../models/salary-calculation-response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalcSalaryService {
+  constructor() {}
 
-  constructor() { }
+  calculateSalary(
+    request: SalaryCalculationRequest
+  ): SalaryCalculationResponse {
+    const regularSalary = request.hourlyWage * request.hoursWorked;
+    const overtimeSalary = request.hourlyWage * 1.5 * request.overtimeHours;
+    const totalSalary = regularSalary + overtimeSalary;
+    const deductions = totalSalary * 0.1;
+    const netSalary = totalSalary - deductions;
+
+    return {
+      regularSalary,
+      overtimeSalary,
+      deductions,
+      netSalary,
+    };
+  }
 }
